4399f276586ff1aebee32212bd8400b5f5ab82fa
Xayvay <Xayvair@gmail.com>
2017-10-26 16:45:10 -0400

    Now I am creating a test case to check for if a specific cell has any neighbors. If it does I will return that neighbor amount. This will help me later with the Game of life rules.

--- conwaysgameoflife/src/test/java/conwaysgameoflife/conwaysTestCases.java
+++ conwaysgameoflife/src/test/java/conwaysgameoflife/conwaysTestCases.java
@@ -9,27 +9,34 @@ import org.junit.Test;
 
 public class conwaysTestCases {
 
+	ConwaysGameOfLife life = new ConwaysGameOfLife();
 
+	int[][] grid = {{0,0,0,0,0,0,1,0},
+					{1,1,1,0,0,0,1,0},
+					{0,0,0,0,0,0,1,0},
+					{0,0,0,0,0,0,0,0},
+					{0,0,0,1,1,0,0,0},
+					{0,0,0,1,1,0,0,0}};
+	
+	int[][]wrong = {{0,1,0},
+					{0,0,0},
+	   				{2,1,4}};
+	
 	@Test
 	public void testAccurateGrid() {
-		ConwaysGameOfLife life = new ConwaysGameOfLife();
+
 		
-		int[][] grid = {{0,0,0,0,0,0,1,0},
-						{1,1,1,0,0,0,1,0},
-						{0,0,0,0,0,0,1,0},
-						{0,0,0,0,0,0,0,0},
-						{0,0,0,1,1,0,0,0},
-						{0,0,0,1,1,0,0,0}};
-		
-		int[][]wrong = {{0,1,0},
-						{0,0,0},
-		   				{2,1,4}};
 		
 		assertEquals(true,life.isAccurateGrid(grid));
 		assertEquals(false,life.isAccurateGrid(wrong));
 		}
 	
-//	@Test
-//	public void test 
+	@Test
+	public void testHowManyNeighbors(){
+		
+		int count = 0;
+		count = life.testHowManyNeighbors(grid,1,1);
+		assertEquals(2,count);
+	}
 	}
 
